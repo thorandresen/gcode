d7698f4ab7decb0a3b4d08744d6206849b5e9765
Thor Garske Andresen <thorg3@gmail.com>
2020-02-03 12:01:18 +0100

    xd

--- src/app/App.java
+++ src/app/App.java
@@ -12,66 +12,33 @@ public class App {
     public static void main(String[] args) throws Exception {
         Generator generator = new Generator();
 
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.2f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.4f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.6f);
-        generator.move(4f, 3f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.2f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.4f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeConstraint(4f, 3f, 7f, 20f, 0.6f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 0.8f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 1.0f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 1.2f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 0.8f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 1.0f);
-        generator.move(4f, 3f);
-        generator.moveAndExtrudeActuator(4f, 3f, 20f, 6.8f, 1.2f);
+        float startX = 10f;
+        float startY = 10f;
+        float length = 80f;
 
-        /*
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 0.2f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 0.2f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 0.4f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 0.4f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 0.6f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 0.6f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 0.8f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 0.8f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 1.0f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 1.0f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 1.2f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 1.2f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 1.4f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 1.4f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 1.6f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 1.6f);
-         * 
-         * generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 10f,
-         * 1.8f); generator.moveAndExtrudeActuator(4f, 10f, 10f, 10f, 1.8f);
-         * 
-         * /*generator.move(4f,3f); generator.moveAndExtrudeConstraint(4f, 3f, 7f, 5f,
-         * 0.6f); generator.move(4f,3f); generator.moveAndExtrudeActuator(4f, 3f, 5f,
-         * 5f, 0.8f); generator.move(9f,8f); generator.moveAndExtrudeConstraint(9f, 8f,
-         * 2.3f, -5f, 0.8f);
-         */
+        /*generator.move(startX, startY);
+        generator.moveAndExtrudeActuator(startX, startY, length, 4f, 0.2f);
+        generator.moveAndExtrudeConstraint(60.4f, startY, 4f, length, 0.2f);*/
 
+
+       
+
+        generator.move(startX, startY);
+        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.2f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.4f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.6f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.8f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.0f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.2f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.4f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.6f);
     }
 
 }
@@ -87,15 +54,15 @@ class Generator {
         float i;
 
         for (i = 0.4f; i < length; i += 0.4f) {
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E1");
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E2.6");
             currentY = yStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.2");
-            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E1");
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.02");
+            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F1000 E2.6");
             i += 0.4f;
             currentY = yStart + i;
-            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E0.2");
+            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F1000 E0.02");
         }
-
+        //lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E1");
         Files.write(file, lines, StandardCharsets.UTF_8);
     }
 
@@ -105,21 +72,24 @@ class Generator {
         float currentX = xStart;
         float i;
 
-        for (i = 0.4f; i < length; i += 0.4f) {
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.2");
+        for (i = 0.4f; i < length-0.4; i += 0.4f) {
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.06");
             currentX = xStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E1");
-            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F600 E0.2");
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.01");
+            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.06");
             i += 0.4f;
             currentX = xStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F600 E1");
+            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.01");
         }
-
+        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.01");
+        currentX = xStart + i;
+        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.06");
+        lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.01");
         Files.write(file, lines, StandardCharsets.UTF_8);
     }
 
     public void move(float x, float y) throws IOException {
-        lines.add("G0" + " X" + x + " Y" + y + " F300");
+        lines.add("G0" + " X" + x + " Y" + y + " F1000");
 
         Files.write(file, lines, StandardCharsets.UTF_8);
     }

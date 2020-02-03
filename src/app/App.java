71e7bcb9b202208f8d73b1437fbb381006790dbc
Thor Garske Andresen <thorg3@gmail.com>
2020-02-03 13:59:58 +0100

    change

--- src/app/App.java
+++ src/app/App.java
@@ -14,31 +14,30 @@ public class App {
 
         float startX = 10f;
         float startY = 10f;
-        float length = 80f;
+        float length = 100f;
 
         /*generator.move(startX, startY);
         generator.moveAndExtrudeActuator(startX, startY, length, 4f, 0.2f);
         generator.moveAndExtrudeConstraint(60.4f, startY, 4f, length, 0.2f);*/
 
-
        
-
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.2f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 0.2f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.4f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 0.4f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.6f);
-        generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.8f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 0.8f);
         generator.move(startX, startY);
         generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.0f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.2f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 1.2f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.4f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 1.4f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.6f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 1.6f);
+        generator.move(startX, startY);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 1.8f);
+       
     }
 
 }
@@ -54,10 +53,10 @@ class Generator {
         float i;
 
         for (i = 0.6f; i < length; i += 0.6f) {
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E2.0");
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E2.5");
             currentY = yStart + i;
             lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.01");
-            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E2.0");
+            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E2.5");
             i += 0.6f;
             currentY = yStart + i;
             lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E0.01");

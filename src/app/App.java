73ff1bf5802888c1b3cca63af806ab2292cb7ade
Thor Garske Andresen <thorg3@gmail.com>
2020-02-03 13:26:18 +0100

    more space

--- src/app/App.java
+++ src/app/App.java
@@ -24,21 +24,21 @@ public class App {
        
 
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.2f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.2f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.4f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.4f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.6f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.6f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeConstraint(startX, startY, 2.2f, length, 0.8f);
+        generator.moveAndExtrudeConstraint(startX, startY, 2f, length, 0.8f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.0f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.0f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.2f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.2f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.4f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.4f);
         generator.move(startX, startY);
-        generator.moveAndExtrudeActuator(startX, startY, length, 2f, 1.6f);
+        generator.moveAndExtrudeActuator(startX, startY, length, 1.8f, 1.6f);
     }
 
 }
@@ -53,14 +53,14 @@ class Generator {
         float currentX = xStart + width;
         float i;
 
-        for (i = 0.4f; i < length; i += 0.4f) {
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E2.6");
+        for (i = 0.6f; i < length; i += 0.6f) {
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E2.0");
             currentY = yStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.02");
-            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F1000 E2.6");
-            i += 0.4f;
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.01");
+            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E2.0");
+            i += 0.6f;
             currentY = yStart + i;
-            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F1000 E0.02");
+            lines.add("G1" + " X" + xStart + " Y" + currentY + " Z" + z + " F600 E0.01");
         }
         //lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E1");
         Files.write(file, lines, StandardCharsets.UTF_8);
@@ -72,20 +72,20 @@ class Generator {
         float currentX = xStart;
         float i;
 
-        for (i = 0.4f; i < length-0.4; i += 0.4f) {
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.03");
+        for (i = 0.6f; i < length-0.6; i += 0.6f) {
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.03");
             currentX = xStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.006");
-            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.03");
-            i += 0.4f;
+            lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.01");
+            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F600 E0.03");
+            i += 0.6f;
             currentX = xStart + i;
-            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.006");
+            lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F600 E0.01");
         }
 
-        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.006");
+        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.01");
         currentX = xStart + i;
-        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F1000 E0.03");
-        lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F1000 E0.006");
+        lines.add("G1" + " X" + currentX + " Y" + currentY + " Z" + z + " F600 E0.03");
+        lines.add("G1" + " X" + currentX + " Y" + yStart + " Z" + z + " F600 E0.01");
         Files.write(file, lines, StandardCharsets.UTF_8);
     }
 
